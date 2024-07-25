bc3c9fddef9af804fa32867a35d978720595445c
Abel Quezada <abelqh9@gmail.com>
2024-07-25 14:55:34 -0500

    feat: add filter by countries on reports

---
+++ frontend/src/components/ReportsCountrySelect/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+import { Checkbox, ListItemText } from "@material-ui/core";
+import Badge from "@material-ui/core/Badge";
+import FormControl from "@material-ui/core/FormControl";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+
+const ReportsCountrySelect = ({
+  countries,
+  selectedCountryIds = [],
+  onChange,
+}) => {
+  const handleChange = (e) => {
+    localStorage.setItem(
+      "ReportsCountrySelect",
+      JSON.stringify(e.target.value)
+    );
+    onChange(e.target.value);
+  };
+
+  return (
+    <Badge
+      badgeContent={selectedCountryIds.length}
+      color="primary"
+      max={99999}
+      invisible={selectedCountryIds.length === 0}
+    >
+      <div style={{ width: 140 }}>
+        <FormControl fullWidth margin="dense">
+          <Select
+            multiple
+            displayEmpty
+            variant="outlined"
+            value={selectedCountryIds}
+            onChange={handleChange}
+            MenuProps={{
+              anchorOrigin: {
+                vertical: "bottom",
+                horizontal: "left",
+              },
+              transformOrigin: {
+                vertical: "top",
+                horizontal: "left",
+              },
+              getContentAnchorEl: null,
+            }}
+            renderValue={() => "Paises"}
+          >
+            {countries?.length > 0 &&
+              countries.map((c) => (
+                <MenuItem dense key={c.id} value={c.id}>
+                  <Checkbox
+                    style={{
+                      color: "black",
+                    }}
+                    size="small"
+                    color="primary"
+                    checked={selectedCountryIds.indexOf(c.id) > -1}
+                  />
+                  <ListItemText primary={c.name} />
+                </MenuItem>
+              ))}
+          </Select>
+        </FormControl>
+      </div>
+    </Badge>
+  );
+};
+
+export default ReportsCountrySelect;
