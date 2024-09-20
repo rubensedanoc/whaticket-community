6050af639133ce9f80e9bee88b001efe81a44e0e
Abel Quezada <abelqh9@gmail.com>
2024-09-20 13:49:29 -0500

    feat: improve contacts cards logic

--- frontend/src/components/TicketListModal/index.js
+++ frontend/src/components/TicketListModal/index.js
@@ -2,8 +2,10 @@ import React, { useContext, useEffect, useState } from "react";
 
 import Dialog from "@material-ui/core/Dialog";
 
+import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
 import TableContainer from "@material-ui/core/TableContainer";
+import NewTicketModal from "../NewTicketModal";
 import TicketListItem from "../TicketListItem";
 
 import CircularProgress from "@material-ui/core/CircularProgress";
@@ -28,6 +30,7 @@ const TicketListModal = ({ modalOpen, onClose, title, tickets, newView }) => {
   const [loading, setLoading] = useState(false);
   const [ticketsData, setTicketsData] = useState([]);
   const { whatsApps } = useContext(WhatsAppsContext);
+  const [newTicketModalOpen, setNewTicketModalOpen] = useState(false);
 
   useEffect(() => {
     console.log("tickets", tickets);
@@ -70,43 +73,71 @@ const TicketListModal = ({ modalOpen, onClose, title, tickets, newView }) => {
   };
 
   return (
-    <Dialog open={modalOpen} onClose={handleClose} maxWidth="lg" scroll="paper">
-      <DialogTitle id="form-dialog-title">{title}</DialogTitle>
-      <DialogContent dividers style={{ width: "900px" }}>
-        <TableContainer component={Paper}>
-          {ticketsData
-            .sort((a, b) => {
-              if (a.clientTimeWaiting > b.clientTimeWaiting) {
-                return 1;
-              }
-              if (a.clientTimeWaiting < b.clientTimeWaiting) {
-                return -1;
-              }
-              return 0;
-            })
-            .map((ticket) => (
-              <div style={{ overflow: "hidden" }} key={ticket.id}>
-                <TicketListItem
-                  ticket={ticket}
-                  key={ticket.id}
-                  openInANewWindowOnSelect={true}
-                />
+    <>
+      <NewTicketModal
+        modalOpen={newTicketModalOpen}
+        onClose={(e) => setNewTicketModalOpen(false)}
+      />
+      <Dialog
+        open={modalOpen}
+        onClose={handleClose}
+        maxWidth="lg"
+        scroll="paper"
+      >
+        <DialogTitle id="form-dialog-title">
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            {title}
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={() => setNewTicketModalOpen(true)}
+            >
+              Crear ticket
+            </Button>
+          </div>
+        </DialogTitle>
+        <DialogContent dividers style={{ width: "900px" }}>
+          <TableContainer component={Paper}>
+            {ticketsData.length ? (
+              ticketsData
+                .sort((a, b) => {
+                  if (a.clientTimeWaiting > b.clientTimeWaiting) {
+                    return 1;
+                  }
+                  if (a.clientTimeWaiting < b.clientTimeWaiting) {
+                    return -1;
+                  }
+                  return 0;
+                })
+                .map((ticket) => (
+                  <div style={{ overflow: "hidden" }} key={ticket.id}>
+                    <TicketListItem
+                      ticket={ticket}
+                      key={ticket.id}
+                      openInANewWindowOnSelect={true}
+                    />
+                  </div>
+                ))
+            ) : (
+              <div style={{ textAlign: "center", padding: "20px" }}>
+                Este contacto no tiene ningun ticket abierto
               </div>
-            ))}
-        </TableContainer>
-        {loading && (
-          <CircularProgress
-            color="primary"
-            size={50}
-            style={{
-              marginLeft: "auto",
-              marginRight: "auto",
-              display: "block",
-            }}
-          />
-        )}
-      </DialogContent>
-    </Dialog>
+            )}
+          </TableContainer>
+          {loading && (
+            <CircularProgress
+              color="primary"
+              size={50}
+              style={{
+                marginLeft: "auto",
+                marginRight: "auto",
+                display: "block",
+              }}
+            />
+          )}
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
