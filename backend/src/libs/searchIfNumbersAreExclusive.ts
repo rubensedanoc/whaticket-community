66bd974512737253ac283fb7dab066271751f065
Abel Quezada <abelqh9@gmail.com>
2024-09-19 19:15:40 -0500

    fix: add try-catch on searchIfNumbersAreExclusive function

--- backend/src/libs/searchIfNumbersAreExclusive.ts
+++ backend/src/libs/searchIfNumbersAreExclusive.ts
@@ -1,46 +1,46 @@
+import * as Sentry from "@sentry/node";
+
 export async function searchIfNumbersAreExclusive({
   numbers
 }: {
   numbers: number[];
 }): Promise<{ [key: string]: boolean }> {
-  const url =
-    "https://microservices.restaurant.pe/backendrestaurantpe/public/rest/common/localbi/searchPhoneList";
+  let result = {};
 
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(numbers)
-  });
-
-  if (!response.ok) {
-    throw new Error(
-      "searchIfNumbersAreExclusive was not ok " + response.statusText
-    );
-  }
-
-  const data = await response.json();
-
-  // console.log(
-  //   "----------------- searchIfNumbersAreExclusive ENTRADA:",
-  //   numbers
-  // );
-
-  if (typeof data.data === "object") {
-    for (const number in data.data) {
-      if (data.data[number].some(o => o.isExclusive)) {
-        data.data[number] = true;
+  try {
+    // ESTA API HACE EL FILTRADO DEL ARRAY QUE LE PASO
+    const response = await fetch(
+      "https://microservices.restaurant.pe/backendrestaurantpe/public/rest/common/localbi/searchPhoneList",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(numbers)
       }
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        "searchIfNumbersAreExclusive was not ok " + response.statusText
+      );
     }
 
-    return data.data;
-  } else {
-    return {};
+    const data = await response.json();
+
+    if (typeof data.data === "object") {
+      for (const number in data.data) {
+        if (data.data[number].some(o => o.isExclusive)) {
+          data.data[number] = true;
+        }
+      }
+
+      result = data.data;
+    }
+  } catch (error) {
+    console.log("--- Error in searchIfNumbersAreExclusive", error);
+    Sentry.captureException(error);
   }
 
-  // console.log(
-  //   "----------------- searchIfNumbersAreExclusive SALIDA:",
-  //   data.data
-  // );
+  return result;
 }
