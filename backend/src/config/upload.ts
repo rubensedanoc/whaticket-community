6bae0978f4fbe651af2023a04b27d1e38dd87895
Abel Quezada <abelqh9@gmail.com>
2024-12-03 11:29:55 -0500

    chore: improve multer config for improve filenames

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,14 +1,34 @@
-import path from "path";
 import multer from "multer";
+import path from "path";
 
 const publicFolder = path.resolve(__dirname, "..", "..", "public");
+
 export default {
   directory: publicFolder,
 
   storage: multer.diskStorage({
     destination: publicFolder,
     filename(req, file, cb) {
-      const fileName = new Date().getTime() + path.extname(file.originalname);
+      let originalName = null;
+
+      // Trata de extraer el nombre original del archivo
+      try {
+        originalName = path.basename(
+          file.originalname,
+          path.extname(file.originalname)
+        );
+      } catch (error) {
+        console.error(
+          "Error al obtener el nombre original del archivo",
+          error,
+          file.originalname
+        );
+      }
+
+      // Combina el nombre original, marca de tiempo y extensión
+      const fileName = `${
+        originalName || "noOriginalName"
+      }-${new Date().getTime()}${path.extname(file.originalname)}`;
 
       return cb(null, fileName);
     }
