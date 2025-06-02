fa177efa99fcf82b7e32c40ead01fc3538ea7585
Abel Quezada <abelqh9@gmail.com>
2025-06-02 15:32:00 -0500

    [feat] remove isCompanyMember validation for calculate beenWaitingSinceTimestamp

--- backend/src/services/TicketServices/getAndSetBeenWaitingSinceTimestampTicketService.ts
+++ backend/src/services/TicketServices/getAndSetBeenWaitingSinceTimestampTicketService.ts
@@ -26,7 +26,7 @@ const getAndSetBeenWaitingSinceTimestampTicketService = async (
     .map(wpp => `'${wpp.number}'`)
     .join(",");
 
-  const sql = `SELECT
+  const sqlConValidacionIsCompanyMember = `SELECT
                 t.id,
                 t.createdAt,
                 t.contactId,
@@ -85,6 +85,69 @@ const getAndSetBeenWaitingSinceTimestampTicketService = async (
                         AND (m.isPrivate IS NULL OR m.isPrivate != 1)
                         AND (m.fromMe = 1
                         OR c.isCompanyMember = '1'
+                        OR c.number IN (${allWhatasappIds}))
+                      THEN m.timestamp END
+                ) as lastCSMessageTimestamp
+              FROM Tickets t
+              INNER JOIN Messages m ON t.id = m.ticketId
+              LEFT JOIN Contacts c ON m.contactId = c.id
+              WHERE t.id in (${ticketsIds})
+              GROUP BY t.id `;
+
+  const sql = `SELECT
+                t.id,
+                t.createdAt,
+                t.contactId,
+                t.status,
+                t.isGroup,
+                t.whatsappId,
+                MIN( CASE
+                      WHEN t.id = m.ticketId
+                        AND (m.isPrivate IS NULL OR m.isPrivate != '1')
+                      THEN m.timestamp
+                     END
+                ) as fistTicketMessageTimestamp,
+                MIN( CASE
+                      WHEN t.id = m.ticketId
+                        AND (m.isPrivate IS NULL OR m.isPrivate != '1')
+                        AND m.fromMe != 1
+                        AND c.number NOT IN (${allWhatasappIds})
+                      THEN m.timestamp
+                     END
+                ) as firstClientMessageTimestamp,
+                ( SELECT MIN( m_inner.timestamp )
+                  FROM Messages m_inner
+                  LEFT JOIN Contacts c_inner ON m_inner.contactId = c_inner.id
+                  WHERE m_inner.ticketId = t.id
+                    AND (m_inner.isPrivate IS NULL OR m_inner.isPrivate != '1')
+                    AND m_inner.fromMe != '1'
+                    AND c_inner.number NOT IN (${allWhatasappIds})
+                    AND m_inner.timestamp >
+                      ( SELECT MAX(mcs.timestamp)
+                        FROM Messages mcs
+                        LEFT JOIN Contacts c_sub ON mcs.contactId = c_sub.id
+                        WHERE mcs.ticketId = t.id
+                          AND ( mcs.body NOT LIKE CONCAT(UNHEX('E2808E'), '%')
+                            AND (mcs.isPrivate IS NULL OR mcs.isPrivate != 1)
+                            AND ( mcs.fromMe = 1
+                                OR c_sub.number IN (${allWhatasappIds})
+                            )
+                          )
+                      )
+                ) as firstLastClientMessageTimestamp,
+                MIN( CASE
+                      WHEN t.id = m.ticketId
+                        AND m.body NOT LIKE CONCAT(UNHEX('E2808E'), '%')
+                        AND (m.isPrivate IS NULL OR m.isPrivate != 1)
+                        AND (m.fromMe = 1
+                        OR c.number IN (${allWhatasappIds}) )
+                      THEN m.timestamp END
+                ) as firstCSMessageTimestamp,
+                MAX( CASE
+                      WHEN t.id = m.ticketId
+                        AND m.body NOT LIKE CONCAT(UNHEX('E2808E'), '%')
+                        AND (m.isPrivate IS NULL OR m.isPrivate != 1)
+                        AND (m.fromMe = 1
                         OR c.number IN (${allWhatasappIds}))
                       THEN m.timestamp END
                 ) as lastCSMessageTimestamp
