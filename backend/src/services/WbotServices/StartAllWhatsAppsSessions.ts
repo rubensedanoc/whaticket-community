edf8c6f16c684d6e40739629aefd9353c5c1ee58
Abel Quezada <abelqh9@gmail.com>
2025-10-23 14:04:43 -0500

    fix(connections): only start no deleted wpps

--- backend/src/services/WbotServices/StartAllWhatsAppsSessions.ts
+++ backend/src/services/WbotServices/StartAllWhatsAppsSessions.ts
@@ -6,7 +6,7 @@ export const StartAllWhatsAppsSessions = async (): Promise<void> => {
   // Aumenta el límite de listeners al inicio del script
   process.setMaxListeners(20);
 
-  const whatsapps = await ListWhatsAppsService();
+  const whatsapps = await ListWhatsAppsService({ showAll: false });
   if (whatsapps.length > 0) {
     whatsapps.forEach(whatsapp => {
       StartWhatsAppSession(whatsapp);
