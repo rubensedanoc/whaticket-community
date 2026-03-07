a364d3fa915bc118d1f7bf8791ac6155ac45a01f
VictorPH <restpevic@gmail.com>
2026-03-07 15:20:55 -0500

    Feat(GetProfilePicsUrl)Obtener fotos de perfil mejorados, ahora cuando. Ahora cuando la librería de WhatsApp falle al obtener la foto de perfil (error "Cannot read properties of undefined (reading 'isNewsletter')"), el sistema igual dejara crear el contacto

--- backend/src/services/WbotServices/GetProfilePicUrl.ts
+++ backend/src/services/WbotServices/GetProfilePicUrl.ts
@@ -1,14 +1,19 @@
 import GetDefaultWhatsApp from "../../helpers/GetDefaultWhatsApp";
 import { getWbot } from "../../libs/wbot";
 
-const GetProfilePicUrl = async (number: string): Promise<string> => {
-  const defaultWhatsapp = await GetDefaultWhatsApp();
+const GetProfilePicUrl = async (number: string): Promise<string | undefined> => {
+  try {
+    const defaultWhatsapp = await GetDefaultWhatsApp();
 
-  const wbot = getWbot(defaultWhatsapp.id);
+    const wbot = getWbot(defaultWhatsapp.id);
 
-  const profilePicUrl = await wbot.getProfilePicUrl(`${number}@c.us`);
+    const profilePicUrl = await wbot.getProfilePicUrl(`${number}@c.us`);
 
-  return profilePicUrl;
+    return profilePicUrl;
+  } catch (err) {
+    console.log(`Error getting profile pic for number ${number}:`, err);
+    return undefined;
+  }
 };
 
 export default GetProfilePicUrl;
