61d7ed68f876bb26981e283cf6fe1f7c6f5ae177
VictorPH <restpevic@gmail.com>
2026-03-14 11:41:45 -0500

    Ft(Report) Se activaron todos los reportes de whaticket

--- backend/src/routes/reports.ts
+++ backend/src/routes/reports.ts
@@ -20,18 +20,24 @@ reportsRoutes.get(
   ReportsController.getATicketsList
 );
 
-// reportsRoutes.get("/reportHistory", ReportsController.reportHistory);
-// reportsRoutes.get(
-//   "/reportHistoryWithDateRange",
-//   ReportsController.reportHistoryWithDateRange
-// );
-// reportsRoutes.get("/reportToExcel", ReportsController.reportToExcel);
-// reportsRoutes.get("/reportToExcelForIA", ReportsController.reportToExcelForIA);
-// reportsRoutes.get("/reportToUsers", ReportsController.reportToUsers);
-// reportsRoutes.get(
-//   "/chatbotMessagesReportToExcel",
-//   ReportsController.chatbotMessagesReportToExcel
-// );
+reportsRoutes.get("/reportHistory", isAuth, ReportsController.reportHistory);
+reportsRoutes.get(
+  "/reportHistoryWithDateRange",
+  isAuth,
+  ReportsController.reportHistoryWithDateRange
+);
+reportsRoutes.get("/reportToExcel", isAuth, ReportsController.reportToExcel);
+reportsRoutes.get(
+  "/reportToExcelForIA",
+  isAuth,
+  ReportsController.reportToExcelForIA
+);
+reportsRoutes.get("/reportToUsers", isAuth, ReportsController.reportToUsers);
+reportsRoutes.get(
+  "/chatbotMessagesReportToExcel",
+  isAuth,
+  ReportsController.chatbotMessagesReportToExcel
+);
 
 // ----------------- comercial reports page
 // reportsRoutes.get(
